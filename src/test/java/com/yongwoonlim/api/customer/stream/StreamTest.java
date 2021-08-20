39e9056a129434c5d3c70c04b35e49a150490372
Yongwoon Lim <eng.yowoo@gmail.com>
2021-08-21 00:17:27 +0900

    스트림 관련 테스트 추가

---
+++ src/test/java/com/yongwoonlim/api/customer/stream/StreamTest.java
@@ -0,0 +1,22 @@
+package com.yongwoonlim.api.customer.stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class StreamTest {
+    @DisplayName("Stream isEmpty() Count")
+    @Test
+    void streamIsEmptyTest(){
+        Stream<String> stream = Arrays.asList("Apple", "Orange", "").stream();
+        int emptyCount = (int)stream.filter(String::isEmpty).count();
+        assertThat(emptyCount, is(equalTo(1)));
+    }
+}
